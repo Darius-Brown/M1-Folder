a8029194c628e0777bb628aeee8cbf6d9243a67d
Darius Brown <Brown.darius.r@gmail.com>
2021-07-28 18:09:20 -0500

    stock

--- StockAPI/Stock.js
+++ StockAPI/Stock.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
+    function stockSearch(){
     const key = "E1HKPP98GQPXLYGB";
+    let userInput = $('#search').val();
 
-    var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=' + key;
+
+    //main url for AlphaVantage
+    let mainUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=' + key;
+
+    //Search url for Alpha
+    let url = 'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=' +userInput+'&apikey='+key;
 
     $.ajax({
         url,
@@ -9,7 +16,18 @@ $(document).ready(function() {
         success: function (data) {
             console.log(data);
 
+            let match = data.bestMatches[0];
+            let name = match.name;
+            console.log(match);
+            console.log(name);
+            $('#result').append(name);
         },
         type: "GET"
     });
+};
+
+$('#submit').on('click',function(e) {
+    stockSearch();
+});
+
 });
