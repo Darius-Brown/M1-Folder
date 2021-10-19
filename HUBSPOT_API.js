820caea7b12690c201d5a4901094e7483cea6add
Darius-Brown <78042299+Darius-Brown@users.noreply.github.com>
2021-10-18 21:04:52 -0500

    Create HUBSPOT_API.js

---
+++ HUBSPOT_API.js
@@ -0,0 +1,14 @@
+// Hubspot api call
+const API_key = "6af57575-f49c-4644-9c94-e55c47b9f04d";
+
+Example DELETE URL:
+https://api.hubapi.com/automation/v3/workflows/342543?hapikey=demo
+
+Returns a 204 No content response on a successful delete.
+
+Returns a 400 error if the workflow ID doesn't exist
+
+https://api.hubapi.com/automation/v3/workflows/29427917?hapikey=6af57575-f49c-4644-9c94-e55c47b9f04d
+
+https://api.hubapi.com/automation/v3/workflows/?hapikey=6af57575-f49c-4644-9c94-e55c47b9f04d
+
